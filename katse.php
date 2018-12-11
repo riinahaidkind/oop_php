1495a295d4592381eb0e02891b293c9aba94c64a
riina.haidkind <riina.haidkind@khk.ee>
2018-12-11 08:42:29 +0200

    Katsetasin php faili tööd.

---
+++ katse.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: riina.haidkind
+ * Date: 06.12.2018
+ * Time: 11:09
+ */
+
+echo '
+    <!doctype html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <h1>PHP abil loodud HTML</h1>
+        </body>
+    </html>
+';
+?>
