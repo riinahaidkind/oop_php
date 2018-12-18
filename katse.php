ca983af5c128f27da6cd1c623e3f188d4e627797
riina.haidkind <riina.haidkind@khk.ee>
2018-12-18 11:03:12 +0200

    Kasutajanimi tuleb sisestada ka!

--- katse.php
+++ katse.php
@@ -1,86 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: riina.haidkind
- * Date: 06.12.2018
- * Time: 11:09
- */
-
-// muutujate defineerimine
-// $muutujaNimi = väärtus;
-
-// massiivid
-// array()
-/*
-$massiiv = array(); // tühi massiiv - ei ole sees midagi
-$massiiv[] = väärtus;
- */
-function suguVarv($sugu){
-    if($sugu == 'naine'){
-        echo '<div style="color: red">';
-    } else {
-        echo '<div style="color: blue">';
-    }
+function vorm(){
+    echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">
+        <div>
+            <label>kasutajanimi :</label>
+            <input type="text" name="username">
+        </div>
+        <input type="submit" value="Saada!">
+    </form>';
 }
-
-function valjastaInfo($massiiv){
-    foreach ($massiiv as $alammassiivNimi => $alamMassiivAndmed){
-        suguVarv($alamMassiivAndmed['sugu']);
-        echo '<h5>'.$alammassiivNimi.'</h5><br>';
-        foreach ($alamMassiivAndmed as $elemendiNimi => $elemendiVaartus){
-            suguVarv($alamMassiivAndmed['sugu']);
-            echo $elemendiNimi.' - '.$elemendiVaartus.'</div>';
+if(empty($_POST)){
+    vorm();
+} else {
+    foreach ($_POST as $element){
+        if(empty($element)){
+            echo 'Andmed tuleb sisestada ka!<br>';
+            echo '<a href="'.$_SERVER['PHP_SELF'].'">Proovi uuesti!</a>';
+            exit;
         }
-        echo '<hr>';
     }
+    echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
+    echo 'Tere tulemast ' . $_POST['username'] . '!<br>';
 }
-function suguVordlus($porsas1, $porsas2){
-    if ($porsas1['sugu'] == $porsas2['sugu']) {
-        return 0; }
-    return ($porsas1['sugu'] < $porsas2['sugu']) ? -1 : 1;
-}
-function sortSooJargi($perekond){
-    usort($perekond, 'suguVordlus');
-}
-// massiivi loomine
-$perekondPeppa = array(
-            'Peppa' => array(
-                'nimi' => 'Peppa',
-                'amet' => 'põrsaslaps',
-                'vanus' => 5,
-                'sugu' => 'naine'
-            ),
-            'George' => array(
-                'nimi' => 'George',
-                'amet' => 'põrsaslaps',
-                'vanus' => 2,
-                'sugu' => 'mees'
-            ),
-            'Ema' => array(
-                'Põrsas Ema' => array(
-                    'nimi' => 'Ema Põrsas',
-                    'amet' => 'põrsasema',
-                    'vanus' => 35,
-                    'sugu' => 'naine'
-                ),
-                'Isa' => array(
-                    'Põrsas Isa' => array(
-                        'nimi' => 'Isa Põrsas',
-                        'amet' => 'põrsasisa',
-                        'vanus' => 40,
-                        'sugu' => 'mees'
-                    )
-                );
-
-//sort($perekondPeppa);
-//asort($perekondPeppa);
-//ksort($perekondPeppa);
-sortSooJargi($perekondPeppa);
-// lehe sisu väljastamine
-echo '<!doctype html><html><head>
-<title>Vormid</title>
- <link rel="stylesheet" type="text/css" href="katsestyle.css">
- </head><body>';
- valjastaInfo($perekondPeppa);
- echo '</body></html>';
- ?>
+?>
