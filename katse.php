c336b8c4e423fc8c8c870ab5a7002fd33e3266ff
riina.haidkind <riina.haidkind@khk.ee>
2018-12-18 11:41:17 +0200

    Lihtne arvuti

--- katse.php
+++ katse.php
@@ -1,24 +1,80 @@
+<style>
+    .number, input[type = "text"]{
+        width: 50px;
+        float: left;
+    }
+    input[type="submit"]{
+
+    }
+    .paneel {
+        width: 130px;
+        height: 75px;
+        padding: 20px;
+        background: lightblue;
+    }
+    .operation {
+        width: 30px;
+        float: left;
+    }
+
+</style>
 <?php
-function vorm(){
+function vorm($tulemus = false){
     echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">
-        <div>
-            <label>kasutajanimi :</label>
-            <input type="text" name="username">
+        <div class="paneel">
+            <div class="number">
+                <input type="text" name="number1">
+            </div>
+            <div class="operation">
+                <select name="operation">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="*">*</option>
+                    <option value="/">/</option>
+                </select>
+            </div>
+            <div class="number">
+                <input type="text" name="number2">
+            </div>
+            <div style="clear: both"></div>
+            <br>
+            <div>
+                <label style="float: left"> Tulemus:</label> 
+                <input type="text" value="'.$tulemus.'">    
+            </div>
+            <br>
+            <input type="submit" value="Saada!">
         </div>
-        <input type="submit" value="Saada!">
+        
     </form>';
+}
+function calc($arv1, $arv2, $operation){
+    if($operation == '+'){
+        $tulemus =  $arv1 + $arv2;
+    } else if ($operation == '-'){
+        $tulemus = $arv1 - $arv2;
+    } else if ($operation == '*'){
+        $tulemus = $arv1 * $arv2;
+    }else if ($operation == '/'){
+        $tulemus = $arv1 / $arv2;
+    } else {
+        $tulemus = false;
+    }
+    return $tulemus;
 }
 if(empty($_POST)){
     vorm();
 } else {
-    foreach ($_POST as $element){
-        if(empty($element)){
-            echo 'Andmed tuleb sisestada ka!<br>';
-            echo '<a href="'.$_SERVER['PHP_SELF'].'">Proovi uuesti!</a>';
-            exit;
-        }
+    $number1 = $_POST['number1'];
+    $number2 = $_POST['number2'];
+    $operation = $_POST['operation'];
+    if(empty($number1) or empty($number2)){
+        echo 'Mõlemad arvud tuleb sisestada!<br>';
+        echo '<a href="'.$_SERVER['PHP_SELF'].'">Proovi uuesti!</a>';
+        exit;
     }
-    echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
-    echo 'Tere tulemast ' . $_POST['username'] . '!<br>';
+    $tulemus = calc($number1, $number2, $operation);
+    vorm($tulemus);
 }
+
 ?>
